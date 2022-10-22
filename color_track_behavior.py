92817b2fc60d7c8c0bc2e8a533e80b4ddc21806c
seth <sethhill7627@gmail.com>
2022-10-21 22:07:17 -0400

    adding in color tracking behavior

--- color_track_behavior.py
+++ color_track_behavior.py
@@ -60,3 +60,67 @@ class ColorTrackingBehavior:
         display_frame = np.concatenate((frame, processed), axis=1) # change axis to 0 to stack frames vertically
         encoded_bytes = camera_stream.get_encoded_bytes_for_frame(display_frame)
         put_output_image(encoded_bytes)
+
+    def process_frame(self, frame):
+        # process frame
+        masked, coordinates, radius = self.find_object(frame)
+        # changes masked image to 3 channel image
+        processed = cv2.cvtColor(masked, cv2.COLOR_GRAY2BGR)
+        # draws same circle on original frame for comparsion on web app
+        cv2.circle(frame, coordinates, radius, [255, 0, 0])
+        self.make_display(frame, processed)
+        return coordinates, radius
+
+    def run(self):
+        # runs the behavior
+        # set servos on pan and tilt camera to 0 position (forward)
+        self.robot.set_pan(0)
+        self.robot.set_tile(0)
+        # setup camera stream
+        camera = camera_stream.setup_camera()
+        # set up pid controllers
+        # pid for speed based on radius
+        speed_pid = PIController(proportional_constant=0.8, integral_constant=0.1, windup_limit=100)
+        # pid for direction based on distance
+        direction_pid = PIController(proportional_constant=0.25, integral_constant=0.05, windup_limit=400)
+
+        # sleep to allow servos and camera to warm up
+        time.sleep(0.1)
+        # stop servos at center position
+        self.robot.servos.stop_all()
+        print('Setup Complete')
+        print('Radius, Radius error, speed value, direction, error, direction value')
+
+        # main loop
+        for frame in camera_stream.start_stream(camera):
+            (x, y), radius = self.process_frame(frame)
+
+            # check for control messages from web app and for objects big enough to look for
+            self.process_control()
+            if self.running and radius > 20:
+                # robot should be moving
+                # run pid loops
+                radius_error = self.correct_radius - radius
+                speed_value = speed_pid.get_value(radius_error)
+                # use center coordinate and current object, x to get direction error
+                direction_error = self.center - x
+                direction_value = direction_pid.get_value(direction_error)
+                print(f"""{radius}, {radius_error}, {speed_value:.2f}, {direction_error},
+                      {direction_error}, {direction_value:.2f}""")
+                # produce motor speeds
+                self.robot.set_left(speed_value - direction_value)
+                self.robot.set_right(speed_value + direction_value)
+            else:
+                # if no object worth examining or stop is sent from web app
+                self.robot.stop_motors()
+                if not self.running:
+                    speed_pid.reset()
+                    direction_pid.reset()
+
+print('Setting up')
+behavior = ColorTrackingBehavior(Robot())
+process = start_server_process('color_track_behavior.html')
+try:
+    behavior.run()
+finally:
+    process.terminate()
