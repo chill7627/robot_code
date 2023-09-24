e4e6d51ae507e0e861f90232d7ad5b1048643a94
seth <sethhill7627@gmail.com>
2023-09-23 20:23:36 -0700

    Made control server a service

--- control_server.py
+++ control_server.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template, jsonify
 from robot_modes import RobotModes
+from leds_led_shim import Leds
 
 
 # build flask app
 app = Flask(__name__)
 mode_manager = RobotModes()
-
+leds = Leds()
+leds.set_one(1, [0, 255, 0])
+leds.show()
 
 # route for template
 @app.route('/')
@@ -16,6 +19,12 @@ def index():
 # set up routes/api endpoints
 @app.route("/run/<mode_name>", methods=['POST'])
 def run(mode_name):
+    # check if leds are on and clear if so, leds just used to show that robot is ready
+    global leds
+    if leds:
+        leds.clear()
+        leds.show()
+        leds = None
     # function that runs when there is a post request to /run/<mode_name> path/url
     mode_manager.run(mode_name)
     response = {"message": f'{mode_name} running'}
